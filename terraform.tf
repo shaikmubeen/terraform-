41620a1702398b8e98757cf326778b8c9f5f5e4e
mubeen shaik <96565287+shaikmubeen@users.noreply.github.com>
2022-04-29 16:45:46 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 #Resources
 #create vpc
-resource "aws_vpc" "vpc1" {
+resource "aws_vpc" "vpc1-1" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_hostnames = "true"
